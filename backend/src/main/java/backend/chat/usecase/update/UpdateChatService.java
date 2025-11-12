d60c49e460dd558a1938060de992d3cbe9c55a50
Seraphin Aeschbach <seae@if.de>
2025-11-12 23:37:15 +0100

    unDownForAllUsers method

--- backend/src/main/java/backend/chat/usecase/update/UpdateChatService.java
+++ backend/src/main/java/backend/chat/usecase/update/UpdateChatService.java
@@ -13,7 +13,6 @@ public class UpdateChatService {
     @Inject
     CriteriaBuilderFactory criteriaBuilderFactory;
 
-
     public void triggerDown(final Long chatId, final Long userId) {
         final var criteriaBuilder = criteriaBuilderFactory.create(entityManager, BLRelChatUser.class);
         criteriaBuilder
@@ -22,5 +21,15 @@ public class UpdateChatService {
         final var bLRelChatUser  = criteriaBuilder.getSingleResult();
         bLRelChatUser.setDowned(!bLRelChatUser.isDowned());
         entityManager.persist(bLRelChatUser);
+    }
+
+    public void unDownForAllUsers(final Long chatId) {
+        final var criteriaBuilder = criteriaBuilderFactory.create(entityManager, BLRelChatUser.class);
+        criteriaBuilder.where("chat.id").eq(chatId);
+        final var bLRelChatUsers  = criteriaBuilder.getResultList();
+        bLRelChatUsers.forEach(bLRelChatUser -> {
+            bLRelChatUser.setDowned(false);
+            entityManager.persist(bLRelChatUser);
+        });
     }
 }
