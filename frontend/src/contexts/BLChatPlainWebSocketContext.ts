9443eb8235459689a9405be3821bdcb2e78e4d43
Seraphin Aeschbach <seae@if.de>
2025-11-15 22:59:48 +0100

    bl chat plain web socket context

---
+++ frontend/src/contexts/BLChatPlainWebSocketContext.ts
@@ -0,0 +1,7 @@
+import { createWebSocketContext } from "./createWebSocketContext.tsx";
+import type { BLChatPlainDto } from '../dtos/BLChatPlainDto.ts';
+
+export const {
+  Provider: BLChatPlainWebSocketProvider,
+  useWebSocketContext: useBLChatPlainWebSocket
+} = createWebSocketContext<BLChatPlainDto>("ws://localhost:8080/chatsocket");
